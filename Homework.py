084d1340fdd38a4d8dc9587ccc19f9e18f85202c
PoeticCrane <poeticcrane@gmail.com>
2022-11-22 17:42:05 +0300

    Exercise #5

--- Homework.py
+++ Homework.py
@@ -72,18 +72,46 @@
 
 # ----------------------------------SOLUTION-----------------------------------
 
-quarter = input("Укажите четверть для отображения  ")
+# quarter = input("Укажите четверть для отображения  ")
 
-quarterRangeX = {
-    '1': " 0 < X < Infinity",
-    '2': " -Infinity < X < 0",
-    '3': " -Infinity < X < 0",
-    '4': " 0 < X < Infinity"}
-quarterRangeY = {
-    '1': " 0 < Y < Infinity",
-    '2': "  0 < Y < Infinity",
-    '3': " -Infinity < Y < 0",
-    '4': " -Infinity < Y < 0"}
+# quarterRangeX = {
+#     '1': " 0 < X < Infinity",
+#     '2': " -Infinity < X < 0",
+#     '3': " -Infinity < X < 0",
+#     '4': " 0 < X < Infinity"}
+# quarterRangeY = {
+#     '1': " 0 < Y < Infinity",
+#     '2': "  0 < Y < Infinity",
+#     '3': " -Infinity < Y < 0",
+#     '4': " -Infinity < Y < 0"}
 
-print(quarterRangeX[quarter])
-print(quarterRangeY[quarter])
+# print(quarterRangeX[quarter])
+# print(quarterRangeY[quarter])
+
+
+# ----------------------------------Exercise #5------------------------------
+# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+# Пример:
+# - A (3,6); B (2,1) -> 5,09
+# - A (7,-5); B (1,-1) -> 7,21
+# ----------------------------------SOLUTION-----------------------------------
+
+frstPoint = list(
+    input("Введите координаты первой точки через пробел ").split())
+scndPoint = list(
+    input("Введите координаты первой точки через пробел ").split())
+print(frstPoint[1])
+print(scndPoint)
+
+
+def distance(fP, scdP):
+    if len(fP) != len(scdP):
+        print("Точки в разных измерениях")
+    else:
+        dist = float(0)
+        for i in range(len(fP)):
+            dist += (int(scdP[i])-int(fP[i]))**2
+        print(f"Прямое расстояние между точками {round(dist**(1/2),2)}")
+
+
+distance(frstPoint, scndPoint)
