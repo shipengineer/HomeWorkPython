a0e36113edc0b29d955500116dd5c74bfcde808f
PoeticCrane <poeticcrane@gmail.com>
2022-11-22 16:54:29 +0300

    Exercise #4

--- Homework.py
+++ Homework.py
@@ -67,3 +67,23 @@
 
 
 # quadrant(x, y)
+# ----------------------------------Exercise #4------------------------------
+# Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+# ----------------------------------SOLUTION-----------------------------------
+
+quarter = input("Укажите четверть для отображения  ")
+
+quarterRangeX = {
+    '1': " 0 < X < Infinity",
+    '2': " -Infinity < X < 0",
+    '3': " -Infinity < X < 0",
+    '4': " 0 < X < Infinity"}
+quarterRangeY = {
+    '1': " 0 < Y < Infinity",
+    '2': "  0 < Y < Infinity",
+    '3': " -Infinity < Y < 0",
+    '4': " -Infinity < Y < 0"}
+
+print(quarterRangeX[quarter])
+print(quarterRangeY[quarter])
