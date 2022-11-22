2cf7d2a3308d454bda6901141d9399e60bc1b5af
PoeticCrane <poeticcrane@gmail.com>
2022-11-22 16:13:22 +0300

    Exercise #2

--- Homework.py
+++ Homework.py
@@ -5,15 +5,31 @@
 # - 7 -> да
 # - 1 -> нет
 # ----------------------------------SOLUTION-----------------------------------
-def holydays():
-    a = int(input("впишите день недели "))
-    if a == 6 or a == 7:
-        print(f"{a} -> да")
-    elif 0 < a < 6:
-        print(f"{a} -> нет")
-    else:
-        print(f"{a} -> введен не день недели")
-        holydays()
+# def holydays():
+#     a = int(input("впишите день недели "))
+#     if a == 6 or a == 7:
+#         print(f"{a} -> да")
+#     elif 0 < a < 6:
+#         print(f"{a} -> нет")
+#     else:
+#         print(f"{a} -> введен не день недели")
+#         holydays()
 
 
-holydays()
+# holydays()
+
+# ----------------------------------Exercise #1------------------------------
+# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат. ¬ - Отрицание ⋁ - логическое "Или" ⋀ - логическое "И"
+fExpression = input("Первая предитата = ")
+sExpression = input("Вторая предитата = ")
+trdExpression = input("Тертья предитата = ")
+
+
+def logicCheck(X, Y, Z):
+    firstHalf = X or Y or Z
+    secondHalf = not X and not Y and not Z
+    check = not firstHalf == secondHalf
+    print(check)
+
+
+logicCheck(fExpression, sExpression, trdExpression)
