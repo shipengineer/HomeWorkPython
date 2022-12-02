1934dd836b6640320d0de0f89b088db916aa7e14
MarineIngenier <poeticcrane@gmail.com>
2022-12-02 09:27:13 +0300

    Exercise #4.2

--- Homework#4.py
+++ Homework#4.py
@@ -3,9 +3,6 @@
 # - при d = 0.001, π = 3.141
 # - при d = 0.0001, π = 3.1415
 import math
-roundNumber = input("Укажите сколько знаков после запятой (Пример: 0.0001) : ")
-
-print(roundNumber)
 
 
 def roundPi(d):
@@ -14,5 +11,28 @@ def roundPi(d):
     print(pi[:count])
 
 
-roundPi(roundNumber)
+# roundNumber = input("Укажите сколько знаков после запятой (Пример: 0.0001) : ")
+# roundPi(roundNumber)
 # ____________________________________________________________________________________________________
+
+# 2'. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
+# * 6 -> [1,2,3]
+# * 144 -> [2,3]
+
+def SimpMult(num):
+    result = [1]
+    i = 2
+    mutable = num
+    while i <= num:
+        if num % i == 0:
+            if i not in result:
+                result.append(i)
+
+            num //= i
+        else:
+            i += 1
+    print(f'Простые множители числа {mutable} : {result}')
+
+
+# Nnum = int(input("Введите число для разложения на простые множители: "))
+# SimpMult(Nnum)
