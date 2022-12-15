fb72477bc1a699de88363366922c0bcacf1c3355
MarineIngenier <poeticcrane@gmail.com>
2022-12-15 15:41:16 +0300

    Progress

--- Homework#7.1/view.py
+++ Homework#7.1/view.py
@@ -5,6 +5,33 @@ def init_book():
 
 
 def view_contacts(data):
+    print("Все контакты")
+    for elem in data:
+        print(elem + "\n")
+
+
+def print_finded(data):
+    for elem in data:
+        print("Вот совпадение:")
+        out = ""
+        for key, value in elem:
+            out = + (key + ':' + value+' ')
+        print(out + '\n')
+
+
+def create_contact_input():
+    return input("Укажите данные новой записи в следующем порядке: Имя Фамилия Телефон Эл.Почта\n Через пробел, пожалуйста: ")
+
+
+def delete_contact_ID():
+    return input("Укажите ID контакта на удаление")
+
+
+def export_question():
+    amount = input("Укажите ID котактов на экспорт")
+    format = input(
+        "Укажите формат вывода данных.\n Доступные форматы вывода: xml, csv")
+    return [amount, format]
 
 
 init_book()
